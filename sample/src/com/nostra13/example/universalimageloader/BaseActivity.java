70ecc299f0ffec8291dd0fcc5e2482f3af4aa70c
장인수(Jang Insoo) <oasisholics@gmail.com>
2013-03-02 17:21:17 +0900

    이미지 부분 다함
    흐핳ㅇ항ㅎㅇㅇ

--- sample/src/com/nostra13/example/universalimageloader/BaseActivity.java
+++ sample/src/com/nostra13/example/universalimageloader/BaseActivity.java
@@ -28,23 +28,7 @@ public abstract class BaseActivity extends Activity {
 
 	protected ImageLoader imageLoader = ImageLoader.getInstance();
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main_menu, menu);
-		return true;
-	}
+	
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-			case R.id.item_clear_memory_cache:
-				imageLoader.clearMemoryCache();
-				return true;
-			case R.id.item_clear_disc_cache:
-				imageLoader.clearDiscCache();
-				return true;
-			default:
-				return false;
-		}
-	}
+	
 }
